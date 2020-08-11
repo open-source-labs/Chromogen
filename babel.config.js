99d7ec7377d4e8e7fe43a2e403432f10b223632e
chenchingk <chenchingk@gmail.com>
2020-08-11 10:17:47 -0700

    Catalyst basic setup

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    // '@babel/preset-typescript',
+  ],
+  // plugins: ['@babel/plugin-proposal-class-properties'],
+};
+
+// 
