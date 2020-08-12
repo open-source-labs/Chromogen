30fc60ae469a38b05439f159ba31cd353abd50c4
Michelle Holland <michellebholland@gmail.com>
2020-08-12 12:30:12 -0700

    refactor: add comments to test file

--- test/selectors.test.js
+++ test/selectors.test.js
@@ -9,7 +9,6 @@ import {
   todoListStatsState,
   refreshFilterState,
 } from '../src/store/store';
-import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
 
 // using react testing library alone might make more sense
 // test('filteredTodoListState should correctly derive state', () => {
@@ -36,20 +35,27 @@ import { useRecoilValue, useRecoilState, useSetRecoilState } from 'recoil';
 //   expect(result.current).toBe(0);
 // });
 
+
 /* Hook to return atom/selector values and/or modifiers 
 for react-recoil-hooks-testing-library */
 
+/* Setup requires:
+  1. *name or key* of every atom and selector, and whether each is read + write or readonly
+  2. labels for value + setter functions – these are user-defined within a component 
+       *** we can either grab those from the app (how?) OR
+       assume them on the basis of convention (e.g. we infer [todoList, setTodoList] from 'todoListState' )
+*/
 const useStoreHook = () => {
+  // atoms - read + write
   const [todoList, setTodoList] = useRecoilState(todoListState);
-  const [todoListFilter, setTodoListFilter] = useRecoilState(
-    todoListFilterState
-  );
+  const [todoListFilter, setTodoListFilter] = useRecoilState(todoListFilterState);
   const [todoListSort, setTodoListSort] = useRecoilState(todoListSortState);
-
+  // selectors - read only
   const filteredTodoList = useRecoilValue(filteredTodoListState);
   const sortedTodoList = useRecoilValue(sortedTodoListState);
   const todoListSortStats = useRecoilValue(todoListSortedStats);
   const todoListStats = useRecoilValue(todoListStatsState);
+  // selector - read + write
   const [refreshFilter, setRefreshFilter] = useRecoilState(refreshFilterState);
 
   return {
@@ -68,8 +74,12 @@ const useStoreHook = () => {
   };
 };
 
-/* ----------- Tests ----------- */
 
+/* Initial state tests require:  
+  1. name/key of atom (used in string passed as first arg to 'test()')
+  2. label for read state value (prop on result.current, passed as arg to expect())
+  3. *captured* inital state value (arg passed to toStrictEqual())  
+*/
 test('todoListState should initialize correctly', () => {
   const { result } = renderRecoilHook(useStoreHook);
   expect(result.current.todoList).toStrictEqual([]);
@@ -80,6 +90,12 @@ test('filteredToDoListState should initialize correctly', () => {
   expect(result.current.filteredTodoList).toStrictEqual([]);
 });
 
+/* Atom update tests require:  
+  1. name/key of atom (used in string passed as first arg to 'test()')
+  2. label for read state value(prop on result.current, passed as arg to expect() AND spread into setter fn)
+  3. label for state setter fn (used in act())
+  4. argument with which setter fn can be invoked (we'd probably need to capture this w wrapper fn/method shadower)
+*/
 test('todoListState should update correctly', () => {
   const { result } = renderRecoilHook(useStoreHook);
 
@@ -95,7 +111,7 @@ test('todoListState should update correctly', () => {
     ]);
   });
 
-  expect(result.current.todoList).toStrictEqual([
+  expect(result.current.todoList).toStrictEqual([ // also need to spread out initial state here
     {
       id: 0,
       test: 'make hamburgers',
@@ -104,7 +120,10 @@ test('todoListState should update correctly', () => {
     },
   ]);
 });
-
+/* Selector tests require:  
+  1. name/key of selector (used in string passed as first arg to 'test()')
+  ...
+*/
 test('filteredTodoState should update correctly', () => {
   const { result } = renderRecoilHook(useStoreHook);
 
@@ -139,3 +158,5 @@ test('filteredTodoState should update correctly', () => {
     },
   ]);
 });
+
+
