0422d3a302fa455ab561d2aa19b69592f3f23fa4
Connor Rose <connorrose.git@gmail.com>
2020-08-12 14:08:41 -0700

    fix(webpack): update entry path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.jsx'),
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
     filename: 'bundle.js',
   },
