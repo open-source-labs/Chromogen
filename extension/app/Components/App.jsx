702aae6b2f8b384c5037f89d0f2e06882a81dea5
Michelle Holland <michelle@MacBook-Pro.local>
2020-08-13 11:30:16 -0700

    build: configure webpack for devtools extension

---
+++ extension/app/Components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div style={{color: 'white'}}>Hi from App.jsx!</div>;
+
+export default App;
---
+++ extension/contentScript.js
@@ -0,0 +1,7 @@
+// send message when content script connects to tab
+// window.postMessage({action: 'contentScriptStarted'}, '*');
+
+// listen for messages from Chromogen package
+
+
+// // listen for messages from the background script
