2caac716c3e32308adb83b03b1a541dc4bf5cc67
Michelle Holland <michellebholland@gmail.com>
2020-08-15 16:02:38 -0700

    fix(package) add selectors to test description

--- package/src/testString.js
+++ package/src/testString.js
@@ -41,9 +41,20 @@ describe('SELECTORS', () => {
   ${snapshots.reduce((tests, { state, selectors }, index) => {
     const updated = state.filter(({ updated }) => updated === true);
     const len = updated.length;
-    return len
-      ? `${tests}it('Selectors should properly derive state when
-       ${updated.slice(0, -1).reduce((list, { key, updated }) => `${list}${key}, `, '')} ${
+    return len && selectors.length
+      ? `${tests}it('${selectors
+          .slice(0, -1)
+          .reduce(
+            (list, { key }, i) => `${list}${key}${i === selectors.length - 2 ? ' ' : ', '}`,
+            '',
+          )}${
+          selectors.length === 1 ? `${selectors[len - 1].key}` : `and ${selectors[len - 1].key}`
+        } should properly derive state when${updated
+          .slice(0, -1)
+          .reduce(
+            (list, { key, updated }, i) => `${list} ${key}${i === len - 2 ? '' : ','}`,
+            '',
+          )} ${
           len === 1 ? `${updated[len - 1].key} updates` : `and ${updated[len - 1].key} update`
         }', () => {
       const { result } = renderRecoilHook(useStoreHook);
