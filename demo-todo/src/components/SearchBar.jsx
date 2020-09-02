0a1be11311d7fa9ac3b7aa64f4d171440a64c442
Andy Wang <andy_wang@comcast.net>
2020-09-02 14:41:41 -0700

    refactor(demo): starting styling

--- demo-todo/src/components/SearchBar.jsx
+++ demo-todo/src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { searchBarSelectorFam } from '../store/store';
 import { useRecoilState } from 'recoil';
+import TodoItem from './TodoItem';
 
 const SearchBar = () => {
   const [searchFilter, setSearchFilter] = useState('all');
@@ -19,13 +20,17 @@ const SearchBar = () => {
         value={searchState.searchTerm}
         onChange={onChange}
       />
-      <select></select>
-
-      <ul>
+      <select>
+        <option value="all">All</option>
+        <option value="high">High Priority</option>
+        <option value="medium">Medium Priority</option>
+        <option value="low">Low Priority</option>
+      </select>
+      <div>
         {searchState.results.map((result, idx) => (
-          <li key={idx}>{result.text}</li>
+          <TodoItem key={idx} item={result} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
