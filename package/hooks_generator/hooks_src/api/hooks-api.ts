c73a3a8452aba42c1c5d1bf488bc2838ab702e32
Cameron Greer <camgreer01@gmail.com>
2020-10-03 16:06:27 -0700

    add code for useState function

--- package/hooks_generator/hooks_src/api/hooks-api.ts
+++ package/hooks_generator/hooks_src/api/hooks-api.ts
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 //import useState and useReducer from React
 //export useState and useReducer as chromogen components
@@ -9,4 +10,33 @@
 3. Track setState (second parameter of useState) function body
 
 */
-import React, { useState } from 'react';
+//Used to grab useState from the React APi
+import React, { useState as reactUseState }  from 'react';
+
+//A function that takes in useState params and pushes them to our ledger, then returns 
+import { updateTracker } from './hooks-core-utils';
+
+//A parameter to test our useState hook
+import { dummyParam } from '../utils/hooks-utils';
+
+//We need ledger to store information the developer passes into useState and setState
+import { ledger } from '../utils/hooks-ledger';
+
+
+//function that user imports
+export function useState(initialState){
+  //bring in the state property from our ledger, which is of type array
+  const { state } = ledger;
+
+  //push our the users intial state into our ledger
+  state.push(initialState)
+
+  //return out a tuple which holds state as the first element and function to update state as the second element
+  return [initialState, updateTracker]
+ 
+
+
+
+}
+
+
