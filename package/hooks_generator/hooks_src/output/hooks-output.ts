1252267e758e17a0a150270c2139f55d59ad54c9
Jinseon Shin <jinseonshin@Jinseons-MacBook-Pro.local>
2020-10-03 15:41:53 -0400

    add pseudocode to hooks-output and create new file hooks-outputlayout

--- package/hooks_generator/hooks_src/output/hooks-output.ts
+++ package/hooks_generator/hooks_src/output/hooks-output.ts
@@ -1,21 +1,27 @@
+/* eslint-disable */
+// import ledger from types
+// import types
+/* eslint-enable */
 
+//transaction = how many times setState cb has fired
 
+//Testing logic for user's state
+//testing for edge cases in the user's app
+
+/* ----- HELPER FUNCTIONS ----- */
+
+/* ----- SETUP FUNCTIONS ----- */
 /*
-Testing logic for user's state
-testing for edge cases in the user's app
+// Create setup functions like importRecoilState,importRecoilFamily, etc for hooks 
+// set up functions to import user's useState and variables related to it: state variable, setState callback function 
+    // use regex to return outcomes 
 
+*/
 
+/* ----- SELECTORS TEST ----- */
+/*
 Recreate hooks version of testSetters in output-utils.ts 
-    //test for string user passes into setState of useState and make sure state is updating 
+    //test for variable user passes into setState of useState and make sure state is updating 
     // don't need to have hook version of testSelectors b/c no get methods 
 
-
-Create setup functions like importRecoilState,importRecoilFamily, etc for hooks 
-
-
-
-
-Will need to scrub key/variables for special characters 
-- REFER TO RECOIL_GENERATOR/output/output.ts & output-utils.ts 
-    - look @ assertState in output-utils.ts; 
-        - create a similar function that will return the setState of useState
+*/
