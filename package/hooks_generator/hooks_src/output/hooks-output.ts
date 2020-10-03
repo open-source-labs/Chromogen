a5f98d3e2e721ef4a6d9db9bdf811512320be41c
Jinseon Shin <jinseonshin@Jinseons-MacBook-Pro.local>
2020-10-03 19:32:04 -0400

    outlined logic for hooks-output.ts file

--- package/hooks_generator/hooks_src/output/hooks-output.ts
+++ package/hooks_generator/hooks_src/output/hooks-output.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import React, { useState } from 'react';
 // import ledger from types
 // import types
 /* eslint-enable */
@@ -13,15 +14,101 @@
 /* ----- SETUP FUNCTIONS ----- */
 /*
 // Create setup functions like importRecoilState,importRecoilFamily, etc for hooks 
-// set up functions to import user's useState and variables related to it: state variable, setState callback function 
-    // use regex to return outcomes 
 
+// set up functions to import user's useState and variables related to it: state variable (array), setState callback function 
+    // use regex to return outcomes
 */
+
+//import hooks state from user's app
+export function importHooksState() {
+  //will be an array
+}
+
+//import hooks callback from user's app
+export function importHooksCallback() {
+  //will be function
+}
+
+//import writeable hook from user's app; MIGHT NOT NEED
+export function writeableHook() {
+  // will be function
+}
+//import readable hook from user's app; MIGHT NOT NEED
+export function readableHook() {
+  // will be function
+}
+
+/* ----- INITIAL RENDER ----- */
+// PROB SHOULD TRACK INITIAL RENDER AND THAT IT EXISTS
+// initialize hooks state
 
 /* ----- SELECTORS TEST ----- */
 /*
 Recreate hooks version of testSetters in output-utils.ts 
     //test for variable user passes into setState of useState and make sure state is updating 
-    // don't need to have hook version of testSelectors b/c no get methods 
 
 */
+
+export function testHooksSetState() {
+  const updatedState;
+
+  let scrubbedParams;
+  //stringify params to make sure there are no special characters or empty spaces
+
+  //CREATE TEST HERE
+
+  //test whether state is set or not set;
+
+  //if state has been set
+  // check whether another setState cb has been fired && previousState.length !== 0 in ledger
+  //if fired, check whether state has changed
+
+  //if state has not been set
+  // check whether setState cb exists
+  //if setState cb exists
+  // check whether setState cb has been fired
+  // if fired
+  //check whether hook state in ledger has been updated
+}
+
+export function testSetters(setTransactionArray: SetTransaction[]): string {
+  return setTransactionArray.reduce((setterTests, { state, setter }) => {
+    const updatedAtoms = state.filter(({ updated }) => updated);
+    if (setter) {
+      const { params } = setter;
+
+      let scrubbedParams;
+      if (typeof params === 'string') {
+        scrubbedParams = params.replace(/[^\w\s]/gi, '');
+      }
+
+      return params !== undefined
+        ? `${setterTests}\tit('${setter.key}__${
+            scrubbedParams !== undefined ? scrubbedParams : JSON.stringify(params)
+          } should properly set state', () => {
+          \t\tconst { result } = renderRecoilHook(useStoreHook);
+          
+          \t\tact(() => {
+          ${initializeAtoms(state, false)}\t\t});
+          
+          \t\tact(() => { 
+          \t\t\tresult.current.set${setter.key}__${
+            scrubbedParams !== undefined ? scrubbedParams : JSON.stringify(params)
+          }(${JSON.stringify(setter.newValue)});
+          \t\t});
+          
+          ${assertState(updatedAtoms)}\t});\n\n`
+        : `${setterTests}\tit('${setter.key} should properly set state', () => {
+            \t\tconst { result } = renderRecoilHook(useStoreHook);
+            
+            \t\tact(() => {
+            ${initializeAtoms(state, false)}\t\t});
+            
+            \t\tact(() => { 
+            \t\t\tresult.current.set${setter.key}(${JSON.stringify(setter.newValue)});
+            \t\t});
+            
+            ${assertState(updatedAtoms)}\t});\n\n`;
+    } else return setterTests;
+  }, '');
+}
--- package/hooks_generator/hooks_src/utils/hooks-ledger.ts
+++ package/hooks_generator/hooks_src/utils/hooks-ledger.ts
@@ -1,3 +1,6 @@
 // Ledger will contain state(current, prev), transactions (will fire when setState is invoked; length > 0)
 
 // import type { HooksLedger } from ""
+
+// initialValue
+// initialRender
