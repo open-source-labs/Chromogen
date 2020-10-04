9cdf242d860188c63aa727b39f787fb55ee7f04d
Cameron Greer <camgreer01@gmail.com>
2020-10-04 09:20:43 -0700

    create trackStateReducer function

--- package/hooks_generator/hooks_src/api/hooks-core-utils.ts
+++ package/hooks_generator/hooks_src/api/hooks-core-utils.ts
@@ -1,35 +1,28 @@
-// similar function logic as wrapSetter (recoil)
-// If transactions length === 0, use closure to reset boolean
-// If we are recording state, and our transactions length > 0, create flag for promise check, debounce, setTimeout to pass in new state value (curr state), return setState function with the user passed in values
-// 
-//returnedPromise, settimeout, return closure function: promise check and debounce, return setState function with the user passed in values
+
 
 import { debounce } from '../utils/hooks-utils';
-import { ledger } from '../utils/hooks-ledger';
+import { hooksLedger } from '../utils/hooks-ledger';
 import { recordingState } from '../utils/hooks-store';
 
 
-const { state,transactions } = ledger
+const { state,setTransactions } = hooksLedger
 
 const DEBOUNCE_MS = 250;
 
-// Set timeout for selector get calls
+// Set timeout for recording new state
 export const debouncedAddToTransactions = debounce(
   (update) => transactions.push({ update }),
   DEBOUNCE_MS,
 );
 
-export const updateTracker = (update) => {
-//instantiate a boolean for our promise check to be false
-//check promise and return update in closure
-  return (newUpdate) => {
-
-    if (recordingState) {
-        debouncedAddToTransactions(newUpdate);
-      }
-    transactions.push(newUpdate);
-    return newUpdate
-  }
+//We are simulating basicStateReducer from React with trackStateReducer
+export const trackStateReducer = (state,action) => {
+  //We push the new state to our ledger, then return it to useReducer in hooks-api
+  const dispatch = typeof action === 'function' ? action(state) : action;
+  //imported from ledger
+  setTransactions.push
+  //We are done tracking, Chromogen passes dispatch to React
+  return dispatch
 }
 
 
