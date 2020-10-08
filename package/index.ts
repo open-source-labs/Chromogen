d657f5401dfad846fe381a5f68c0b3d7f20ae275
cgreer011 <68936597+cgreer011@users.noreply.github.com>
2020-10-07 19:11:10 -0700

    correct id in HooksChromogenObserver (#42)

--- package/index.ts
+++ package/index.ts
@@ -2,7 +2,7 @@
 import { atom, selector, atomFamily, selectorFamily } from './recoil_generator/src/api/api';
 import { ChromogenObserver } from './recoil_generator/src/component/ChromogenObserver';
 import { useState } from './hooks_generator/hooks_src/api/hooks-api'
-import { hooksChromogenObserver } from './hooks_generator/hooks_src/component/HooksChromogenObserver';
+import { HooksChromogenObserver } from './hooks_generator/hooks_src/component/HooksChromogenObserver';
 // CHROMGOEN FAMILY APIs ARE CURRENTLY UNSTABLE
 // import { atomFamily, selectorFamily } from 'recoil';
-export { atom, selector, atomFamily, selectorFamily, ChromogenObserver, hooksChromogenObserver, useState };
+export { atom, selector, atomFamily, selectorFamily, ChromogenObserver, HooksChromogenObserver, useState };
