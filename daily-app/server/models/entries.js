7e27a45a476fe4c1f97bc551082ce61cf49cd049
Lina <woorin.lee1524@gmail.com>
2022-04-23 15:03:45 -0700

    professionalized codebase, removed server-side logic

--- daily-app/server/models/entries.js
+++ daily-app/server/models/entries.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const EntriesSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    default: 'dtalmaraz@gmail.com'
-  },
-  answer: {
-    type: String,
-    required: true,
-  },
-})
+// const EntriesSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     default: 'dtalmaraz@gmail.com'
+//   },
+//   answer: {
+//     type: String,
+//     required: true,
+//   },
+// })
 
-module.exports = mongoose.model('entries', EntriesSchema);
+// module.exports = mongoose.model('entries', EntriesSchema);
