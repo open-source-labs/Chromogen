f6f7c045d38c670614e7104c595e289464067e0c
Dani Almaraz <dtalmaraz@gmail.com>
2022-04-26 00:40:37 -0700

    added todo portion of demo hooks app and styling
    Co-authored-by: Erica Oh <ericaysoh@gmail.com>
    Co-authored-by: Lina Lee <woorin.lee1524@gmail.com>
    Co-authored-by: Dani Almaraz <dtalmaraz@gmail.com>

--- daily-app/client/containers/MainContainer.tsx
+++ daily-app/client/containers/MainContainer.tsx
@@ -3,30 +3,24 @@ import { useState as hooksUseState } from '../../../package/hooks_generator/hook
 const MainContainer: React.FC = () => {
 
   const [subtract, setSubtract] = hooksUseState<number>(0, 'subtract');
-  const [count, setCount] = hooksUseState<number[]>([0], 'count');
+  const [add, setAdd] = hooksUseState<number[]>([0], 'add');
 
   return (
 
-  <div>
-    <p>You clicked {count} times</p>
-    <button onClick={() => {
-        setCount([count[0] + 1])
-      }
-    }>
-    Count
-    </button>
-
-     <p>You subtracted {subtract} times</p>
-      <button onClick={() => {
-        setSubtract(subtract - 1)
-      }
-    }>Subtract</button> 
-
-      <p>You did not </p>
-      <button onClick={() => {      
-      }}>
-        Don't click me
-      </button>
+  <div className='main'>
+    <header>
+      <h1>Score Card</h1>
+    </header>
+    <div className='buttonBox'>
+    <div className='buttonDiv'>
+      <p><span className="num">{add}</span></p>
+      <button className="mainButton" onClick = {() => setAdd([add[0] + 1])}>Add One</button>        
+    </div>
+    <div className='buttonDiv'>
+      <p><span className="num"> {subtract}</span></p>
+      <button className="mainButton" onClick = {() => setSubtract(subtract - 1)}>Subtract One</button>
+    </div>
+    </div>
   </div>
   )
 }
