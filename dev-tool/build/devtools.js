37d04b24a3ffd7b99b4eb4dd081b3d2f7aba3439
Lina <woorin.lee1524@gmail.com>
2022-04-27 18:54:35 -0700

    updated manifest and package.json

--- dev-tool/build/devtools.js
+++ dev-tool/build/devtools.js
@@ -1,4 +1,4 @@
 /* This file is included as a script tag in devtools.html.
    Its only purpose is to create the devtools panel. */
-chrome.devtools.panels.create('Chromogen', null, 'panel.html', null);
+chrome.devtools.panels.create('Chromogen 3.0', null, 'panel.html', null);
 // args - panel title, icon, html, and a callback function.
