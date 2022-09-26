d7d31e4d16876048d4922faaa4ea5958dc48248e
crgb0s <ccboswell120@gmail.com>
2022-09-26 14:35:05 -0400

    fix: bug for invalid hooks call

--- package/index.ts
+++ package/index.ts
@@ -3,5 +3,7 @@ import { atom, selector, atomFamily, selectorFamily } from './recoil_generator/s
 import { ChromogenObserver } from './recoil_generator/src/component/ChromogenObserver';
 import { useState } from './hooks_generator/hooks_src/api/hooks-api'
 import { HooksChromogenObserver } from './hooks_generator/hooks_src/component/HooksChromogenObserver';
+import { create } from './zustand_generator/src/api/api';
+
 // CHROMGOEN FAMILY APIs ARE CURRENTLY UNSTABLE
-export { atom, selector, atomFamily, selectorFamily, HooksChromogenObserver, ChromogenObserver, useState };
+export { atom, selector, atomFamily, selectorFamily, HooksChromogenObserver, ChromogenObserver, useState, create };
