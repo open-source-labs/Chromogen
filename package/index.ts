b8f9f6a792e4109a08260777933608c9f0d6bc3c
crgb0s <ccboswell120@gmail.com>
2022-10-11 16:53:11 -0400

    cleaned up unfinished features

--- package/index.ts
+++ package/index.ts
@@ -1,10 +1,7 @@
 /* eslint-disable */
 import { atom, selector, atomFamily, selectorFamily } from './recoil_generator/src/api/api';
-import { ChromogenZustandObserver } from './zustand_generator/src/component/ChromogenObserver';
+import { ChromogenZustandObserver } from './zustand_generator/src/component/ChromogenZustandObserver';
 import { ChromogenObserver } from './recoil_generator/src/component/ChromogenObserver';
-
-import { useState } from './hooks_generator/hooks_src/api/hooks-api';
-import { HooksChromogenObserver } from './hooks_generator/hooks_src/component/HooksChromogenObserver';
 import { chromogenZustandMiddleware } from './zustand_generator/src/api/api';
 
 // CHROMGOEN FAMILY APIs ARE CURRENTLY UNSTABLE
@@ -13,9 +10,7 @@ export {
   selector,
   atomFamily,
   selectorFamily,
-  HooksChromogenObserver,
   ChromogenObserver,
-  useState,
   chromogenZustandMiddleware,
   ChromogenZustandObserver,
 };
