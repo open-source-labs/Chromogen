3af5c17c1a4e623ae641caa0bfa3c8fe4de9fa20
dnvt <francois@dnvt.me>
2023-02-27 15:44:55 -0500

    Test 1

---
+++ package/zustand_generator/src/component/Buttons/Button.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import RecordButton from './notButtons/RecordButton';
+// import RecordButton from './notButtons/RecordButton';
+import StartButton from './notButtons/StartButton';
+
+const Button = () => {
+  const [isRecording, setIsRecording] = useState(true);
+  const handleClick = () => setIsRecording(!isRecording);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: '20px',
+        width: '100%',
+      }}
+    >
+      {isRecording ? (
+        <RecordButton handleClick={handleClick} />
+      ) : (
+        <StartButton handleClick={handleClick} />
+      )}
+    </div>
+  );
+};
+
+export default Button;
