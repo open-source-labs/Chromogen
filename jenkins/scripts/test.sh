10e4db9edec2a27a804d2b673ff47a326397631c
Your Name <you@example.com>
2023-02-28 13:52:43 -0500

    asdf

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -17,4 +17,4 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-npm test
+npm run test
