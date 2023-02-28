be471bea108a4ca6d464a2f5aa6af01a0f76aed8
Your Name <you@example.com>
2023-02-28 13:42:22 -0500

    asdfasd

--- package/jenkins/scripts/deliver.sh
+++ package/jenkins/scripts/deliver.sh
@@ -28,4 +28,4 @@ set +x
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+echo 'created your initial Pipeline as a Jenkinsfile..)'
