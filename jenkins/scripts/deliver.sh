f954a403e4efd90731769f44cef85967dd16bbfd
Your Name <you@example.com>
2023-02-28 14:00:39 -0500

    asadfasdfd

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -18,7 +18,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+npm --prefix ./package start &
 sleep 1
 echo $! > .pidfile
 set +x
